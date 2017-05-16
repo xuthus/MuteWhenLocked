f033014c3c0a9255bb88856734adfd67ec402c31
sergey.yanzin <xuthus@yandex.ru>
2017-05-17 00:01:43 +0300

    add support for Windows 10 session state

--- src/utils.pas
+++ src/utils.pas
@@ -7,40 +7,97 @@ interface
 uses
   Classes, SysUtils, Windows;
 
-function IsWorkstationLocked: boolean;
-function GetMasterVolume: single;
-procedure SetMasterVolume(const Value: single);
+function IsWorkstationLocked: Boolean;
+function GetMasterVolume: Single;
+procedure SetMasterVolume(const Value: Single);
 procedure InitVolumeEndpoint;
+function IsWindows10orHigher: Boolean;
 
 
 implementation
 
 uses
-  MMDevAPI, ActiveX;
+  MMDevAPI, ActiveX, JwaWinBase, JwaWtsApi32, JwaLmServer, JwaLmApiBuf;
 
 var
   endpointVolume: IAudioEndpointVolume;
+  isWindows10: Boolean;
 
-function IsWorkstationLocked: boolean;
+function IsSessionLocked(): Boolean;
+const
+  WTS_CURRENT_SERVER_HANDLE = 0;
+  WTSSessionInfoEx = 25;
+  WTS_SESSIONSTATE_LOCK = $00000000;
+  WTS_SESSIONSTATE_UNLOCK = $00000001;
+  WTS_SESSIONSTATE_UNKNOWN = $FFFFFFFF;
+type
+  WTSINFOEX = packed record
+    Level: DWORD;
+    SessionID: Int64;
+    ConnectSessionState: DWORD;
+    SessionFlags: Int32;
+  end;
+  PWTSINFOEX = ^WTSINFOEX;
+
+var
+  processId: DWORD;
+  sessionId: DWORD;
+  sessionInfo: PWTSINFOEX;
+  res: BOOL;
+  bytesReturned: DWORD;
+begin
+  Result := False;
+  processId := GetCurrentProcessId;
+  if ProcessIdToSessionId(processId, sessionId) then
+  begin
+    try
+      if WTSQuerySessionInformation(WTS_CURRENT_SERVER_HANDLE,
+        sessionId, WTS_INFO_CLASS(WTSSessionInfoEx), sessionInfo, bytesReturned) then
+        Result := sessionInfo^.SessionFlags = WTS_SESSIONSTATE_LOCK;
+    finally
+      WTSFreeMemory(sessionInfo);
+    end;
+  end;
+end;
+
+function IsWorkstationLocked: Boolean;
 var
   hDesktop: HDESK;
 begin
   Result := False;
-  hDesktop := OpenDesktop('default', 0, False, DESKTOP_SWITCHDESKTOP);
-  try
-    if hDesktop <> 0 then
-      Result := not SwitchDesktop(hDesktop);
-  finally
-    CloseDesktop(hDesktop);
+  if isWindows10 then
+    Result := IsSessionLocked()
+  else
+  begin
+    hDesktop := OpenInputDesktop(0, False, DESKTOP_SWITCHDESKTOP);
+    try
+      if hDesktop <> 0 then
+        Result := not SwitchDesktop(hDesktop);
+    finally
+      CloseDesktop(hDesktop);
+    end;
   end;
 end;
 
-function GetMasterVolume: single;
+function IsWindows10orHigher: Boolean;
+var
+  Buffer: PServerInfo101;
+begin
+  Buffer := nil;
+  if NetServerGetInfo(nil, 101, Pointer(Buffer)) = NO_ERROR then
+    try
+      Result := Buffer^.sv101_version_major >= 10;
+    finally
+      NetApiBufferFree(Buffer);
+    end;
+end;
+
+function GetMasterVolume: Single;
 begin
   endpointVolume.GetMasterVolumeLevelScaler(Result);
 end;
 
-procedure SetMasterVolume(const Value: single);
+procedure SetMasterVolume(const Value: Single);
 begin
   endpointVolume.SetMasterVolumeLevelScalar(Value, nil);
 end;
@@ -58,4 +115,6 @@ begin
     nil, endpointVolume);
 end;
 
+initialization
+  isWindows10 := IsWindows10orHigher();
 end.
